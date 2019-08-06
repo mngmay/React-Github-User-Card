49004c5fe2c825d92d1d86ee302ce1894160f943
May Ng <mngmay@gmail.com>
2019-08-06 15:51:09 -0700

    styled user details in card component

--- user-card/src/components/Card.js
+++ user-card/src/components/Card.js
@@ -10,11 +10,9 @@ import Collapse from "@material-ui/core/Collapse";
 import Avatar from "@material-ui/core/Avatar";
 import IconButton from "@material-ui/core/IconButton";
 import Typography from "@material-ui/core/Typography";
-import { red } from "@material-ui/core/colors";
-import FavoriteIcon from "@material-ui/icons/Favorite";
-import ShareIcon from "@material-ui/icons/Share";
+import { teal } from "@material-ui/core/colors";
+import PersonIcon from "@material-ui/icons/Person";
 import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
-import MoreVertIcon from "@material-ui/icons/MoreVert";
 
 export default function UserCard(props) {
   const { follower } = props;
@@ -41,19 +39,35 @@ export default function UserCard(props) {
         <CardMedia
           className={classes.media}
           image={follower.avatar_url}
-          title="Paella dish"
+          title="User Avatar"
         />
         <CardContent>
-          <Typography variant="body2" color="textSecondary" component="p">
+          <Typography variant="body2" color="textPrimary" component="p">
             {follower.bio}
+          </Typography>
+          <Typography
+            variant="body2"
+            color="textSecondary"
+            component="p"
+            className="card-details"
+          >
+            <span>
+              Location:{" "}
+              {follower.location ? follower.location : "Not Available"}
+            </span>
+            <span>Followers: {follower.followers}</span>
+            <span>Following: {follower.following}</span>
           </Typography>
         </CardContent>
         <CardActions disableSpacing>
-          <IconButton aria-label="add to favorites">
-            <FavoriteIcon />
-          </IconButton>
-          <IconButton aria-label="share">
-            <ShareIcon />
+          <IconButton aria-label="Go to Github">
+            <a
+              href={follower.html_url}
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <PersonIcon />
+            </a>
           </IconButton>
           <IconButton
             className={clsx(classes.expand, {
@@ -72,6 +86,7 @@ export default function UserCard(props) {
               <img
                 src={`http://ghchart.rshah.org/${follower.login}`}
                 width="470"
+                alt="User GitHub Activity Graph"
               />
             </Typography>
           </CardContent>
@@ -100,6 +115,6 @@ const useStyles = makeStyles(theme => ({
     transform: "rotate(180deg)"
   },
   avatar: {
-    backgroundColor: red[500]
+    backgroundColor: teal[200]
   }
 }));
